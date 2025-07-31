c8f4ea4c2322b29bb1ef93627bbd816b0fd6469f
Sam Estep <sam@samestep.com>
2025-07-30 22:58:06 -0400

    Add Nix flake with dev shell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShellNoCC {
+          buildInputs = [
+            pkgs.clang-tools # Must come before `pkgs.clang` for clangd to work.
+
+            pkgs.clang
+            pkgs.nixfmt-rfc-style
+            pkgs.paps
+            pkgs.poppler-utils
+            pkgs.tree
+          ];
+        };
+      }
+    );
+}
